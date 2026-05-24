36f1d9aa796b1e91973a07162e3aac40a90a65d7
Raghav10022005 <raghavtiwari1002@gmail.com>
2026-05-24 15:03:38 +0530

    Raghav Tag Portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,14 @@ import "./index.css";
 
 import Lenis from "lenis";
 
+import TagManager from "react-gtm-module";
+
+const tagManagerArgs = {
+  gtmId: "GTM-WXPTPNXC",
+};
+
+TagManager.initialize(tagManagerArgs);
+
 const lenis = new Lenis({
   duration: 1.2,
   smoothWheel: true,
